089df3d12e2e815ffaafc64ae62c952eab016ca8
Amir <amir.devel@gmail.com>
2025-04-25 20:54:27 +0330

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Head from 'next/head';
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -28,9 +28,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-      <meta name="google-adsense-account" content="ca-pub-3389355875819710">
-      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
